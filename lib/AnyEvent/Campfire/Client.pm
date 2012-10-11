51252d50b2792af36c7f2a42533f5ae84ee69df6
Hyungsuk Hong <aanoaa@gmail.com>
2012-10-11 17:03:28 +0900

    tidied

--- lib/AnyEvent/Campfire/Client.pm
+++ lib/AnyEvent/Campfire/Client.pm
@@ -1,6 +1,6 @@
 package AnyEvent::Campfire::Client;
 
-# Abstract:
+# Abstract: Campfire API in an event loop
 use Moose;
 use namespace::autoclean;
 
@@ -22,94 +22,104 @@ has 'account' => (
 sub BUILD {
     my $self = shift;
 
-    if ( !$self->authorization || !scalar @{ $self->rooms } || !$self->account) {
+    if ( !$self->authorization || !scalar @{ $self->rooms } || !$self->account )
+    {
         print STDERR
           "Not enough parameters provided. I Need a token, rooms and account\n";
         exit(1);
     }
 
     for my $room ( @{ $self->rooms } ) {
-        my $uri = sprintf "https://%s.campfirenow.com/room/$room/join", $self->account;
+        my $uri = sprintf "https://%s.campfirenow.com/room/$room/join",
+          $self->account;
         my $scope = AnyEvent::HTTP::ScopedClient->new($uri);
-        $scope->header({
-            Authorization => $self->authorization,
-            Accept => '*/*',
-        })
-            ->post(
-                sub {
-                    my ($body, $hdr) = @_;
-                    if ($hdr->{Status} !~ m/^2/) {
-                        $self->emit('error', "$hdr->{Status}: $hdr->{Reason}");
-                        return;
-                    }
-
-                    $self->emit('join', $room);
-
-                    my $stream = AnyEvent::Campfire::Stream->new(
-                        token => $self->token,
-                        rooms => join(',', @{ $self->rooms }),
-                    );
-
-                    $stream->on('stream', $self->_events->{message}[0]);
-                    $stream->on('error', $self->_events->{error}[0]);
+        $scope->header(
+            {
+                Authorization => $self->authorization,
+                Accept        => '*/*',
+            }
+          )->post(
+            sub {
+                my ( $body, $hdr ) = @_;
+                if ( $hdr->{Status} !~ m/^2/ ) {
+                    $self->emit( 'error', "$hdr->{Status}: $hdr->{Reason}" );
+                    return;
                 }
-            );
+
+                $self->emit( 'join', $room );
+
+                my $stream = AnyEvent::Campfire::Stream->new(
+                    token => $self->token,
+                    rooms => join( ',', @{ $self->rooms } ),
+                );
+
+                $stream->on( 'stream', $self->_events->{message}[0] );
+                $stream->on( 'error',  $self->_events->{error}[0] );
+            }
+          );
     }
 }
 
 sub speak {
-    my ($self, $room, $text) = @_;
+    my ( $self, $room, $text ) = @_;
 
-    my $scope = AnyEvent::HTTP::ScopedClient->new(sprintf "https://%s.campfirenow.com/room/$room/speak", $self->account);
-    $scope->header({
-            Authorization => $self->authorization,
-            Accept => '*/*',
+    my $scope = AnyEvent::HTTP::ScopedClient->new(
+        sprintf "https://%s.campfirenow.com/room/$room/speak",
+        $self->account );
+    $scope->header(
+        {
+            Authorization  => $self->authorization,
+            Accept         => '*/*',
             'Content-Type' => 'application/json',
-        })
-        ->post(
-            encode_json({ message => { body => $text } }),
-            sub {
-                my ($body, $hdr) = @_;
-                if (!$body || $hdr->{Status} !~ m/^2/) {
-                    $self->emit('error', $hdr->{Reason});
-                    return;
-                }
+        }
+      )->post(
+        encode_json( { message => { body => $text } } ),
+        sub {
+            my ( $body, $hdr ) = @_;
+            if ( !$body || $hdr->{Status} !~ m/^2/ ) {
+                $self->emit( 'error', $hdr->{Reason} );
+                return;
             }
-        );
+        }
+      );
 }
 
 sub leave {
-    my ($self, $room) = @_;
-    my $scope = AnyEvent::HTTP::ScopedClient->new(sprintf "https://%s.campfirenow.com/room/$room/leave", $self->account);
-    $scope->header({
-            Authorization => $self->authorization,
-            Accept => '*/*',
+    my ( $self, $room ) = @_;
+    my $scope = AnyEvent::HTTP::ScopedClient->new(
+        sprintf "https://%s.campfirenow.com/room/$room/leave",
+        $self->account );
+    $scope->header(
+        {
+            Authorization  => $self->authorization,
+            Accept         => '*/*',
             'Content-Type' => 'application/json',
-        })
-        ->post(
-            sub {
-                my ($body, $hdr) = @_;
-                if (!$body || $hdr->{Status} !~ m/^2/) {
-                    $self->emit('error', $hdr->{Reason});
-                    return;
-                }
-
-                my $data;
-                try {
-                    $data = decode_json($body);
-                } catch {
-                    $self->emit('error', $_);
-                    return;
-                };
-
-                $self->emit('leave', $data);
+        }
+      )->post(
+        sub {
+            my ( $body, $hdr ) = @_;
+            if ( !$body || $hdr->{Status} !~ m/^2/ ) {
+                $self->emit( 'error', $hdr->{Reason} );
+                return;
             }
-        );
+
+            my $data;
+            try {
+                $data = decode_json($body);
+            }
+            catch {
+                $self->emit( 'error', $_ );
+                return;
+            };
+
+            $self->emit( 'leave', $data );
+        }
+      );
 }
 
 sub exit {
     my $self = shift;
-    for my $room (@{ $self->rooms }) {
+    for my $room ( @{ $self->rooms } ) {
         $self->leave($room);
     }
 }
